cd371d6203957cac31a38f099abf08b235c8ffb6
andresjara <andresjara640@gmail.com>
2017-10-16 18:27:12 -0500

    manejo de layout y el controlador

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
+  layout 'ejemplo'
   def index
     @aprendiz = 'Harold Andres Jara'
-    @limite = 10
+    @limite = 1
     @mensaje = 'estoy haciendo mi primer curso de ruby on rails'
   end
 end
